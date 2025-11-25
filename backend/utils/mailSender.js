52680168038dd39f0a50a49d9fef7de24bac92e4
Shakib Ansari <shakibansari7406@gmail.com>
2025-11-26 01:36:21 +0530

    Improve Mail Sender Code

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -4,9 +4,8 @@ const nodemailer = require("nodemailer");
 // Configure email transporter
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  secure: false,
+  secure: true,
   port: process.env.EMAIL_PORT,
-  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
