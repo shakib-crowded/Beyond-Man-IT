39f44f819fc734cbe46accddc01f7908388d64c7
Shakib Ansari <shakibansari7406@gmail.com>
2025-11-26 11:59:48 +0530

    Change some config in sendOtp

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 // Configure email transporter
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  secure: true,
   port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
